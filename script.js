6c88b85bc537a691e0e106d6cd99c33182582249
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-01-09 09:17:53 -0330

    Character length prompt

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Get references to the #generate element (DOM ELEMENT)
 var generateBtn = document.querySelector("#generate");
+
+// Arrays for Password Criteria
+var specialChar = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+var numericChar = ["0123456789"];
+var lowercaseChar = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercaseChar = ["ABCDEFGHIJKLMNOPQRSTUVYWXYZ"];
+
 
 // Write password to the #password input
 function writePassword() {
@@ -12,6 +16,49 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// Function to generate password
+function generatePassword() {
+  
+  var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var numericChar = "0123456789"; 
+  var lowercaseChar = "abcedfghijklmnopqrstuvwyxz";
+  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var 
+
+  // Character length prompt
+  var charLength = prompt("How many characters would you like your password to contain?");
+
+  // Character length validation - do we have to assume after the first alert that they will enter the correct response in the second attempt? 
+  if (charLength < 8 || charLength > 128) {
+    prompt("Password must contain at least 8 characters. Please try again.");
+  } else if (isNaN(charLength)) {
+    charLength = prompt("Please enter a valid number.");
+  } else {
+      alert("Your password will be " + charLength + " characters.");
+  }
+
+  // Lowercase confirm
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
