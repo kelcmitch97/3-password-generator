3b46017ffce64d503739f902117fb197eb18b9a7
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-01-10 19:31:01 -0330

    Final password for loop created

--- script.js
+++ script.js
@@ -1,81 +1,83 @@
 // Get references to the #generate element (DOM ELEMENT)
 var generateBtn = document.querySelector("#generate");
 
-// Arrays for Password Criteria
-var specialChar = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
-var numericChar = ["0123456789"];
-var lowercaseChar = ["abcdefghijklmnopqrstuvwxyz"];
-var uppercaseChar = ["ABCDEFGHIJKLMNOPQRSTUVYWXYZ"];
-
-
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); // storing whatever is being returned from the generate password function 
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; 
 
 }
 
 // Function to generate password
 function generatePassword() {
   
-  var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var numericChar = "0123456789"; 
-  var lowercaseChar = "abcedfghijklmnopqrstuvwyxz";
-  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  
+  var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+  var numericChar = "0123456789".split("");
+  var lowercaseChar = "abcedfghijklmnopqrstuvwyxz".split("");
+  var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  var getRandomPassword = [ ];
+  var finalPassword = " ";
+
 
   // Character length prompt
   var charLength = prompt("How many characters would you like your password to contain?");
 
   // Character length validation - do we have to assume after the first alert that they will enter the correct response in the second attempt? 
+ 
   if (charLength < 8 || charLength > 128) {
-    prompt("Password must contain at least 8 characters. Please try again.");
+    prompt("Password must contain between 8 and 128 characters. Please try again.");
   } else if (isNaN(charLength)) {
-    charLength = prompt("Please enter a valid number.");
-  } else {
-      alert("Your password will be " + charLength + " characters.");
-  }
+    charLength = prompt("Please enter a valid number between 8 and 128 characters.");
+  } 
+
+  alert("Your password will be " + charLength + " characters.");
 
   // Lowercase confirm prompt
-  lowercaseChar = confirm("Do you want to include lowercase characters?");
+  var getLowercaseChar = confirm("Do you want to include lowercase characters?");
 
-  // Lowercase confirm validation - Do i need this? 
-  if (lowercaseChar) {
+  // Lowercase confirm validation 
+  if (getLowercaseChar) {
     alert("Your password will have lowercase characters.");
+    getRandomPassword = getRandomPassword.concat(lowercaseChar);
   } 
 
    // Uppercase confirm prompt
-   uppercaseChar = confirm("Do you want to include uppercase characters?");
+   var getUppercaseChar = confirm("Do you want to include uppercase characters?");
 
    // Uppercase confirm validation
-   if (uppercaseChar) {
+   if (getUppercaseChar) {
      alert("Your password will have uppercase characters.");
+     getRandomPassword = getRandomPassword.concat(uppercaseChar);
    } 
 
   //  Numeric numbers confirm prompt
-  numericChar = confirm("Do you want to include numeric characters?");
+  var getNumericChar = confirm("Do you want to include numeric characters?");
 
   // Numeric numbers validation
-  if (numericChar) {
+  if (getNumericChar) {
     alert("Your password will have numeric characters.");
+    getRandomPassword = getRandomPassword.concat(numericChar);
   }
 
   // Special characters confirm prompt
-  specialChar = confirm("Do you want to include special characters?");
+  var getSpecialChar = confirm("Do you want to include special characters?");
 
-  if (specialChar) {
+  if (getSpecialChar) {
     alert("Your password will have special characters.")
+    getRandomPassword = getRandomPassword.concat(specialChar);
   }
 
-  // For loop to get random characters and numbers based on charLength 
-    var getRandomLowercase = "";
 
+  // For loop to get random characters and numbers based on charLength 
+    
     for (var i = 0; i < charLength; i++) {
-      getRandomLowercase += lowercaseChar[Math.floor(Math.random() * lowercaseChar.length)];
+      finalPassword += getRandomPassword[Math.floor(Math.random() * getRandomPassword.length)];
     }
-   console.log(getRandomLowercase);
+
+    return finalPassword;
+
   }
 
 
